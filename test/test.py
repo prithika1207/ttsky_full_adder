cb15a19cc03b3dae4e03fca7063fd5ca57f2a941
prithika1207 <prithikapradhaban0@gmail.com>
2026-04-25 11:09:07 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -2,26 +2,23 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import cocotb
-from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles
+from cocotb.triggers import Timer
+
 
 
 @cocotb.test()
-async def test_project(dut):
-    dut._log.info("Start")
+async def test_full_adder(dut):
+    dut._log.info("Starting Gate-Level Hardened Simulation...")
+ 
 
-    # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, unit="us")
-    cocotb.start_soon(clock.start())
 
-    # Reset
-    dut._log.info("Reset")
     dut.ena.value = 1
     dut.ui_in.value = 0
     dut.uio_in.value = 0
     dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
+    await Timer(50,unit="ns")
     dut.rst_n.value = 1
+    await Timer (50,unit = "ns")
 
     dut._log.info("Test project behavior")
 
